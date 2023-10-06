20446026db9573cf70e1b96864931b014cc9574a
Øystein Gisnås <oystein@gisnas.net>
2023-10-06 06:28:56 +0200

    Filter unchanged values

--- comfobridge/ventilation.py
+++ comfobridge/ventilation.py
@@ -4,8 +4,10 @@ from aiocomfoconnect import ComfoConnect
 
 class Ventilation:
     def __init__(self, comfoconnect_host, uuid, local_uuid, sensor_callback):
-        self.comfoconnect = ComfoConnect(comfoconnect_host, uuid, sensor_callback=sensor_callback)
+        self.comfoconnect = ComfoConnect(comfoconnect_host, uuid, sensor_callback=self.filter_unchanged)
         self.local_uuid = local_uuid
+        self.sensor_callback_fn = sensor_callback
+        self.last_value = {}
 
     async def connect(self):
         await self.comfoconnect.connect(self.local_uuid)
@@ -23,3 +25,10 @@ class Ventilation:
 
     async def disconnect(self):
         await self.comfoconnect.disconnect()
+
+    def filter_unchanged(self, sensor, value):
+        last_value = self.last_value.get(sensor.name)
+
+        if type(last_value) != type(value) or abs((last_value - value) / last_value) >= 0.02:
+            self.last_value[sensor.name] = value
+            self.sensor_callback_fn(sensor, value)
